9195cc40863ff7df08bf83c790f1a7e017af338e
jbfrrr <jbaferrer@gmail.com>
2017-04-04 00:26:50 +0800

    Update install.sh to add .zshrc and fix minor source (reload) bug

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ -d ~/.dotfiles/ ]; then DIR=~/.dotfiles; else DIR=~/dotfiles; fi
 OLD_DIR=~/.dotfiles_bak;
-FILES=".bashrc .bash_aliases .vimrc .vim";
+FILES=".bashrc .bash_aliases .vimrc .vim .zshrc";
 
 #Create backup folder
 echo "Creating $OLD_DIR for backing up dotfiles in ~";
@@ -11,13 +11,14 @@ echo "...done";
 
 #Move old dotfiles in ~ to backup dir and create symlinks at ~ using the new dotfiles
 for FILE in $FILES; do
-    echo "Moving $FILE from ~ to $OLD_DIR ..."
-    mv ~/$FILE $OLD_DIR/.
-    echo "Symlinking $FILE from $DIR to ~ ..."
-    ln -s $DIR/$FILE ~/$FILE
+    if [ -f ~/$FILE ]
+    then
+	echo "Moving $FILE from ~ to $OLD_DIR ...";
+	mv ~/$FILE $OLD_DIR/;
+    fi
+    echo "Symlinking $FILE from $DIR to ~ ...";
+    ln -s $DIR/$FILE ~/$FILE;
 done
 
 source ~/.bashrc
 source ~/.bash_aliases
-source ~/.vimrc
-
